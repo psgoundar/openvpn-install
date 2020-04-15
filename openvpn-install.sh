fdf07030f655ac328bf0551dce66f4cb462b688e
Swamy Goundar <psg@southecho.com>
2020-04-14 20:27:26 -0700

    Commit

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -924,6 +924,7 @@ tls-cipher $CC_CIPHER
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 client-config-dir /etc/openvpn/ccd
 status /var/log/openvpn/status.log
 verb 3" >>/etc/openvpn/server.conf
@@ -959,6 +960,14 @@ echo "log-append /var/log/openvpn.log" >> /etc/openvpn/server.conf
 =======
 
 >>>>>>> Misc
+=======
+client-config-dir /etc/openvpn/ccd
+status /var/log/openvpn/status.log 20
+verb 3" >> /etc/openvpn/server.conf
+
+	# Create client-config-dir dir
+	mkdir -p /etc/openvpn/ccd
+>>>>>>> Commit
 	# Create log dir
 	mkdir -p /var/log/openvpn
 
